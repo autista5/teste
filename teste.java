f953523d02d32e4969e4bfe60a4a086314067e01
autista5 <skibidisigmatoiletmewing@gmail.com>
2026-02-17 23:05:16 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,14 @@
+let dinheiro = 0;
+const cu = document.querySelector("h1");
+cu.textContent = "bosta h7mana";
+
+const l = document.getElementById("lilo");
+l.width = 099;
+
+function funcao(){
+    
+   dinheiro += 1; 
+    let txt = "dinheiro: "+ dinheiro
+   cu.textContent = txt;
+    
+}
